6ad5c93588c93e721183e41d17ae69a7d6f9526f
a lan <bluenowte97@gmail.com>
2018-11-12 03:04:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
 
-  def initialize
-    @name
-  end
-
-  def name=(the_person_name)
+  def initialize(the_person_name)
     @name = the_person_name
   end
-  def name
-    @name
-  end
+  # 
+  # def name=(the_person_name)
+  #   @name = the_person_name
+  # end
+  # def name
+  #   @name
+  # end
 end
